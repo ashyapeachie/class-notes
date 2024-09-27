bff6363eed31c330eabfa004637d41df064b27ec
Ashya <128183143+ashyapeachie@users.noreply.github.com>
2024-09-26 22:34:56 -0400

    Update ComputeAngels.java

--- ComputeAngels.java
+++ ComputeAngels.java
@@ -1,7 +1,11 @@
 /*
  * author: Ashya
  * filename: ComputeAngels.java
+ * description: the goal is to write a program that prompts the user to enter the x and y 
+ * coordinates of the 3 corner points of a triangle and then display the triangle's angles
+ * important note: I know my file says "Angels" instead of "Angles", I am mindly dyslexic :,) 
  */
+
 package ComputeAngels;
 import java.util.Scanner;
 
@@ -29,16 +33,21 @@ public class ComputeAngels {
         x3 = s.nextDouble();
         System.out.println("please enter y3: \n");
         y3 = s.nextDouble();
-        
+
+        //section below computes three sides
         double a = Math.sqrt((x2 - x3) * (x2 - x3) + (y2 - y3) * (y2 - y3));
         //square root of (x2 - x3) squared plus (y2-y3) squared 
         double b = Math.sqrt((x1 - x3) * (x1 - x3) + (y1 - y2) * (y1 - y2));
+        //square root of 
         double c = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
-        
+        //
+
+        //section below computes three angles
         double A = Math.toDegrees(Math.acos((a * a - b * b - c * c) / (-2 * b * c)));
         double B = Math.toDegrees(Math.acos((b * b - a * a - c * c) / (-2 * a * c)));
         double C = Math.toDegrees(Math.acos((c * c - b * b - a * a) / (-2 * a * c)));
-        
+
+        //section below displays results to the user
         System.out.println("The three angles are: ");
         System.out.printf("A = %.2f \n", A);
         System.out.printf("B = %.2f \n", B);
